1231cb952cc51bc48484c3593053ef92387abecf
susoroh1 <susoroh@venturseed.com>
2023-11-24 07:31:00 +0100

    updated my app

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,25 @@
-
-import { FaTimes, FaEdit } from 'react-icons/fa';
 import { useContext } from 'react';
 import Card from './shared/Card';
 import FeedbackContext from '../context/FeedbackContext';
+import { FaTimes, FaEdit } from 'react-icons/fa'; // Import the necessary icon components
 
 function FeedbackItem({ item }) {
-  const { deleteFeedback , editFeedback} = useContext(FeedbackContext)
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
- 
+  if (!item || !item.rating) {
+    return <div>Loading or no data available</div>;
+  }
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
 
       <button onClick={() => deleteFeedback(item.id)} className='close'>
-        <FaTimes color='purple' />
+        <FaTimes color='purple' /> {/* Render FaTimes icon */}
       </button>
-      <button  onClick={() => editFeedback(item)} className='edit'><FaEdit color='purple' /></button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' /> {/* Render FaEdit icon */}
+      </button>
 
       <div className="text-display">{item.text}</div>
     </Card>
